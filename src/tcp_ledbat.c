fdeb4226ce9157be1f6b4b87b650fe533b5e5e2f
snowmohan <252203262@qq.com>
2017-11-13 21:25:16 -0500

    Update tcp_ledbat.c

--- src/tcp_ledbat.c
+++ src/tcp_ledbat.c
@@ -25,9 +25,9 @@
 #define  DEBUG_BASE_HISTO    1
 
 /* NOTE: len are the actual length - 1 */
-static int base_histo_len = 10;
+static int base_histo_len = 100;
 static int noise_filter_len = 4;
-static int target = 100000;
+static int target = 100;
 static int gain_num = 1;
 static int gain_den = 1;
 static int do_ss = 0;
